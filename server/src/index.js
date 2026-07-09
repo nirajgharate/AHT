9fb63279e93c566fd5f75facf0ea5507bbead774
nirajgharate <nirajgharate.dev@gmail.com>
2026-07-09 15:36:20 +0530

    index file updates

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: CLIENT_ORIGIN || "http://localhost:5173",
+    origin: "https://aht-one.vercel.app" || "http://localhost:5173",
   }),
 );
 app.use(express.json({ limit: "5mb" }));
